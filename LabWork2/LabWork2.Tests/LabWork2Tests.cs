117353699666562f116021870640886db63d324a
EVedmid <55056300+EVedmid@users.noreply.github.com>
2022-01-17 15:34:39 +0200

    lab_1-6

---
+++ LabWork2/LabWork2.Tests/LabWork2Tests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ClassLibrary_LabWork2;
+using NUnit.Framework;
+
+namespace LabWork2.Tests
+{
+    [TestFixture]
+    public class LabWork2Tests
+    {
+        [TestCase(1, 2, 3, 4, 5, 6)]
+        [TestCase(0)]
+        public void ConstructorCount_Get_ShouldReturnCorrectValue(params int[] elements)
+        {
+            //arrange
+            CustomLL<int> list = new CustomLL<int>(elements);
+
+            //act
+            var expected = elements.Length;
+            var actual = list.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual, message: "Count works incorrectly");
+            Assert.AreEqual(elements[0], list[0], message: "Get index works incorrectly");
+        }
+
+        [Test]
+        public void Count_Return0()
+        {
+            //arrange
+            int actual = 0;
+
+            //act
+            CustomLL<int> list = new CustomLL<int>();
+
+            //assert
+            Assert.AreEqual(list.Count, actual, message: "Count works incorrectly");
+        }
+
+        [Test]
+        public void Clear_ReturnCount0()
+        {
+            //arrange
+            CustomLL<int> list = new CustomLL<int>(1, 2, 3);
+            Item<int> expectedHead = null;
+
+            //act
+            list.Clear();
+            int count = list.Count;
+            var actualHead = list.Head;
+
+            //arrange
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(0, count, message: "Clear method works incorrectly ");
+                Assert.AreEqual(expectedHead, actualHead, message: "Clear method works incorrectly ");
+            });
+        }
+
+        [TestCase(2, new int[] { 1, 2, 3, 4, 5 }, 4, true)]
+        [TestCase(10, new int[] { 1, 2, 3, 4, 5 }, 5, false)]
+        [TestCase(15, new int[] { 11, 4, 5 }, 3, false)]
+        [TestCase(-1, new int[] { -1, 2, -1, 4, 5 }, 4, true)]
+        [TestCase(99, new int[] { -1, 2, -1, 4, 100 }, 5, false)]
+        [TestCase(1, new int[] { 1 }, 0, true)]
+        public void Remove_Element_ReturnedExpectedCount(int elementToRemove, int[] elements, int expectedCount, bool expectedBool)
+        {
+            //arrange
+            CustomLL<int> list = new CustomLL<int>(elements);
+
+            //act
+            var actualBool = list.Remove(elementToRemove);
+
+            //arrange
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedCount, list.Count, message: "Remove method works incorrectly");
+                Assert.AreEqual(expectedBool, actualBool, message: "Remove method works incorrectly");
+            });
+        }
+
+
+    }
+}
---
+++ lab_3_4_5/Lab3/WebApplication2/Controllers/GuestController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Provider;
+using BLL.Service;
+using WebApplication2.Models;
+using AutoMapper;
+using System.Net.Http;
+using System.Web;
+using System.Net;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApplication2.Controllers
+{
+    [EnableCors("AnotherPolicy")]
+    [ApiController]
+    [Route("[controller]")]
+    public class GuestController : ControllerBase
+    {
+        private readonly IGuestService guestService = DependencyProvider.GetDependency<IGuestService>();
+        private readonly IPostService postService = DependencyProvider.GetDependency<IPostService>();
+
+        [HttpGet]
+        public IEnumerable<GuestVm> Get()
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<GuestDto, GuestVm>()).CreateMapper();
+            var list = mapper.Map<IEnumerable<GuestDto>, List<GuestVm>>(guestService.GetAll());
+            return list;
+        }
+
+        [HttpGet("{login}")]
+        public GuestVm Get(string login)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<GuestDto, GuestVm>()).CreateMapper();
+            var user = mapper.Map<GuestDto, GuestVm>(guestService.FindByLogin_(login));
+            if (user == null) throw new ArgumentException(new string("login error"));
+            return user;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post ([FromBody] GuestVm guestVm)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<GuestVm, GuestDto>()).CreateMapper();
+            var user = mapper.Map<GuestVm, GuestDto>(guestVm);
+            try
+            {
+                guestService.SignUp(user);
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete("{login}")]
+        public HttpResponseMessage Delete(string login)
+        {
+            try
+            {
+                guestService.Delete_(login);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
